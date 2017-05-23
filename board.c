7de4a67045ef1642c50f15833444cc45a3f264d1
Julián Maestri <serpi90@gmail.com>
2017-05-23 09:07:06 -0300

    Split header files

--- board.c
+++ board.c
@@ -1,62 +1,5 @@
 #include <stdio.h>
-
-/**
- * Store a tic-tac-toe board using an unsigned short (16 bits)
- * First 15 bits are used to encode cell state in base 3
- * Last bit can be used to store whatever you wish
- *
- * Keep in mind that the board should be initialized with EMPTY_BOARD (0)
- * */
-
-/* 
- * ceiling( ln_2( 3^9 -1 ) ) = 15 (bytes used to store information)
- * 2^15 -1 = 32767 (the mask to use)
- *
- * As sizeof(unsigned short) * 8 = 16 we have an extra bit to spare.
- */
-
-/* Type Definitions */
-typedef unsigned short board_t;
-#define EMPTY_BOARD 0
-#define BOARD_MASK 32767
-enum position_t { TOP_LEFT=0, TOP_CENTER=1, TOP_RIGHT=2, CENTER_LEFT=3, CENTER_CENTER=4, CENTER_RIGHT=5, BOTTOM_LEFT=6, BOTTOM_CENTER=7, BOTTOM_RIGHT=8 };
-enum cell_status_t { EMPTY=0, CROSS=1, CIRCLE=2 };
-
-/* Cell manipulation functions */
-enum cell_status_t getCell( board_t board, enum position_t position );
-board_t setCell ( board_t board, enum position_t position, enum cell_status_t newStatus );
-/* Spare bit manipulation functions */
-unsigned char getLastBit ( board_t board );
-board_t setLastBit ( board_t board, unsigned char extra );
-
-/* Implementation */
-unsigned char getLastBit ( board_t board ) {
-	return board & ~BOARD_MASK ? 1 : 0;
-}
-
-board_t setLastBit ( board_t board, unsigned char extra ) {
-	if( extra ) {
-		return ( board & BOARD_MASK ) | ~BOARD_MASK;
-	} else {
-		return board & BOARD_MASK;
-	}
-}
-
-enum cell_status_t getCell( board_t board, enum position_t position ) {
-	board &= BOARD_MASK;
-	while( position-- ) board /= 3;
-	return board % 3;
-}
-
-board_t setCell ( board_t board, enum position_t position, enum cell_status_t newStatus ) {
-	board_t extra = board & ~BOARD_MASK;
-	unsigned short oldStatus = getCell(board, position);
-	while( position-- ) {
-		oldStatus *= 3;
-		newStatus *= 3;
-	}
-	return ( ( board & BOARD_MASK ) - oldStatus + newStatus ) | extra;
-}
+#include "board.h"
 
 /* Printing functions */
 char cellValue( enum position_t value ) {
@@ -70,13 +13,17 @@ char cellValue( enum position_t value ) {
 
 void printBoard( unsigned short board ) {
 	printf("+-+-+-+\n|%c|%c|%c|\n|%c|%c|%c|\n|%c|%c|%c|\n+-+-+-+\nLastBit: %hu\n\n",
-			cellValue(getCell(board, TOP_LEFT)), cellValue(getCell(board, TOP_CENTER)), cellValue(getCell(board, TOP_RIGHT)),
-			cellValue(getCell(board, CENTER_LEFT)), cellValue(getCell(board, CENTER_CENTER)), cellValue(getCell(board, CENTER_RIGHT)),
-			cellValue(getCell(board, BOTTOM_LEFT)), cellValue(getCell(board, BOTTOM_CENTER)), cellValue(getCell(board, BOTTOM_RIGHT)),
+			cellValue(getCell(board, TOP_LEFT)),
+			cellValue(getCell(board, TOP_CENTER)),
+			cellValue(getCell(board, TOP_RIGHT)),
+			cellValue(getCell(board, CENTER_LEFT)),
+			cellValue(getCell(board, CENTER_CENTER)),
+			cellValue(getCell(board, CENTER_RIGHT)),
+			cellValue(getCell(board, BOTTOM_LEFT)),
+			cellValue(getCell(board, BOTTOM_CENTER)),
+			cellValue(getCell(board, BOTTOM_RIGHT)),
 			getLastBit( board ) );
 }
-
-
 
 int main() {
 	board_t board = EMPTY_BOARD;
